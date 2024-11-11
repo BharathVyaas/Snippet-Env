a244465ae1833e4af0cd4adf7dad8b74b72bc8c4
BharathVyaas <132135805+BharathVyaas@users.noreply.github.com>
2024-11-11 13:11:41 +0530

    Update monacoCard.js
    update userNav to display username properly

--- src/app/components/body/monacoCard.js
+++ src/app/components/body/monacoCard.js
@@ -6,7 +6,7 @@ function MonacoCard(props) {
   return (
     <div className="w-[60%] max-w-[600px] h-[440px] shadow-xl rounded-md bg-background-card text-text-primary">
       <div className="">
-        <CardNav setSnippets={props.setSnippets} />
+        <CardNav setSnippets={props.setSnippets} username={props.username} />
       </div>
 
       <div className="">
