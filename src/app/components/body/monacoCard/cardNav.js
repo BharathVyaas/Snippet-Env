0c9b5aed4d61760b853dca89c586ff32d5f190a5
BharathVyaas <132135805+BharathVyaas@users.noreply.github.com>
2024-11-11 13:10:53 +0530

    Update cardNav.js
    update to match user name in cardNav

--- src/app/components/body/monacoCard/cardNav.js
+++ src/app/components/body/monacoCard/cardNav.js
@@ -3,19 +3,19 @@
 import { useSession } from "next-auth/react";
 import React from "react";
 
-function CardNav() {
+function CardNav({ username }) {
   const { data: session } = useSession();
 
   return (
     <div className="border-b border-text-secondary px-3 py-2 flex space-x-1 items-center">
       <img
         className="rounded-3xl border-2 border-border-light"
-        width={24}
-        height={24}
+        width={30}
+        height={30}
         src={session?.user?.image || null}
         alt={session?.user?.name}
       />
-      <p className="text-sm">{session?.user?.name || "placeholder"}</p>
+      <p className="text-sm">{username || "placeholder"}</p>
     </div>
   );
 }
